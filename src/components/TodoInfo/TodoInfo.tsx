5dfbf8cfc160a182d16091963df2ed2d5774b294
Max <maksim.shaggy@gmail.com>
2026-05-13 22:52:23 +0300

    add changes

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,5 +1,9 @@
 import { TodoInfoProps } from '../../types';
 
 export const TodoInfo = ({ todo }: TodoInfoProps) => {
-  return <h2 className="TodoInfo__title">{todo.title}</h2>;
+  return (
+    <div className="TodoInfo">
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+    </div>
+  );
 };
